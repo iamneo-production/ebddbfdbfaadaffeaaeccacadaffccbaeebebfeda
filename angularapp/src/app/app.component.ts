829298e5a80d341869f8c75cc6da424da7be39f6
Iamneo <techadmin@iamneo.ai>
2023-08-08 19:11:29 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit{
   ngOnInit(): void {
     for(let i=0;i<this.total.length;i++)
     {
