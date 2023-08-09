4c174dd5eab2f6dd0e5f7350edeaf88fdbd74b63
Iamneo <techadmin@iamneo.ai>
2023-08-09 20:04:49 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilterPipe } from './filter.pipe';
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- angularapp/src/app/filter.pipe.ts
+++ angularapp/src/app/filter.pipe.ts
@@ -5,20 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any,filter:string){
-    if(filter == '')
-    return value;
-
-    const foods = [];
-    for(let v of value)
-    {
-      if(v.name.includes(filter))
-      {
-        foods.push(v);
-      }
-    }
-
-    return foods;
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
   }
 
 }
